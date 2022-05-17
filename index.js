fcf42ec202114ac8b8f33fe3cb999810823df7e5
Parkwochang <whwh3560@gmail.com>
2022-05-17 15:23:45 +0900

    password hidden

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+const config = require("./config/key");
 const app = express();
 const port = 3000;
 
+//application / x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//applrication / json
+app.use(bodyParser.json());
+
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    `mongodb+srv://woochang:whdwls0305@boilerplate.kc5gn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-`
-  )
+  .connect(config.mongoURI)
   .then(() => {
     console.log("MongoDB Connected...");
   })
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("hi"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get("/", (req, res) => res.send("열렸다!"));
+
+app.post("/register", (req, res) => {
+  //회원 가입할 때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body); // bodyparser가 있기에 가능
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true }); // status(200)은 성공을 의미 성공하면 true 리턴
+  }); //mongoDB에서 오는 save
+}); // 회원가입을 위한 route
+
+app.listen(port, () =>
+  console.log(`Example app Listening on http://localhost:3000`)
+);
